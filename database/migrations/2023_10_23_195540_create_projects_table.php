79179bb34748b58aa2b0fe88fb0acee8f6320956
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-10-24 09:08:27 +0200

    add migrate and seed Project

--- database/migrations/2023_10_23_195540_create_projects_table.php
+++ database/migrations/2023_10_23_195540_create_projects_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('repo');
+            $table->text('description');
             $table->timestamps();
         });
     }

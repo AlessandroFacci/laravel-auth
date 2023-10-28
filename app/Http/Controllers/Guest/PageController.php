4813154e3ad5e5027efbe38b43fe4be5e842824d
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-10-28 22:49:07 +0200

    edit homepage guest

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
   public function index()
   {
-    $title = "Homepage";
-    return view('guest.home', compact('title'));
+    $title = "Projects";
+    $projects = Project::orderByDesc('created_at')->paginate('12');
+
+    return view('guest.home', compact('title', 'projects'));
   }
 }
